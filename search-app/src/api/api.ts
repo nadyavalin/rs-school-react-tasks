8b2176f8cbf85eb022b2371aaf90a8f3d83d5530
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-07-09 22:50:36 +0300

    refactor: remove repeated code

--- search-app/src/api/api.ts
+++ search-app/src/api/api.ts
@@ -1,11 +1,6 @@
 import { PeopleResponse } from "../types/types";
 
-export const fetchSearchPeople = async (searchTerm = ""): Promise<PeopleResponse> => {
+export const fetchPeople = async (searchTerm = ""): Promise<PeopleResponse> => {
   const response = await fetch(`https://swapi.dev/api/people/?search=${searchTerm}`);
-  return response.json();
-};
-
-export const fetchAllPeople = async (searchTerm = ""): Promise<PeopleResponse> => {
-  const response = await fetch(`https://swapi.dev/api/people/${searchTerm}`);
   return response.json();
 };
--- search-app/src/components/main/mainContent.tsx
+++ search-app/src/components/main/mainContent.tsx
@@ -4,66 +4,56 @@ import { SearchForm } from "./searchSection/searchForm";
 import { PeopleList } from "./resultsSection/peopleList";
 import { getItemFromLocalStorage } from "../../utils/utils";
 import { IPerson, PeopleResponse } from "../../types/types";
-import { fetchAllPeople, fetchSearchPeople } from "../../api/api";
+import { fetchPeople } from "../../api/api";
 import { Pagination } from "./resultsSection/pagination";
 
 export const MainContent = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
   const [person, setPerson] = useState<IPerson[]>([]);
   const [people, setPeople] = useState<PeopleResponse | null>(null);
-  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     fetchData(getItemFromLocalStorage<string>("searchTerm") ?? "");
-  }, []);
-
-  useEffect(() => {
     const getPeople = async (searchTerm?: string) => {
       setIsLoading(true);
-      const data: PeopleResponse = await fetchAllPeople(searchTerm);
+      const data: PeopleResponse = await fetchPeople(searchTerm);
       setPeople(data);
       setIsLoading(false);
-      console.log(data);
     };
     getPeople();
   }, []);
-
-  const nextPage = async () => {
-    if (people && people.next) {
-      setIsLoading(true);
-      const response = await fetch(people.next);
-      const data: PeopleResponse = await response.json();
-      setPeople(data);
-      setIsLoading(false);
-      setCurrentPage(currentPage + 1);
-      setPerson(data.results);
-    }
-  };
-
-  const prevPage = async () => {
-    if (people && people.previous) {
-      setIsLoading(true);
-      const response = await fetch(people.previous);
-      const data: PeopleResponse = await response.json();
-      setPeople(data);
-      setIsLoading(false);
-      setCurrentPage(currentPage - 1);
-      setPerson(data.results);
-    }
-  };
 
   const fetchData = async (searchTerm?: string) => {
     try {
       setIsLoading(true);
       setErrorMessage("");
-      const data: PeopleResponse = await fetchSearchPeople(searchTerm);
+      const data: PeopleResponse = await fetchPeople(searchTerm);
       setPerson(data.results);
     } catch (error) {
       setErrorMessage((error as Error).message);
     } finally {
       setIsLoading(false);
     }
+  };
+
+  const fetchDataByPage = async (pageUrl: string, pageNumberChange: number) => {
+    setIsLoading(true);
+    const response = await fetch(pageUrl);
+    const data: PeopleResponse = await response.json();
+    setPeople(data);
+    setIsLoading(false);
+    setCurrentPage(currentPage + pageNumberChange);
+    setPerson(data.results);
+  };
+
+  const nextPage = async () => {
+    fetchDataByPage(people?.next || "", 1);
+  };
+
+  const prevPage = async () => {
+    fetchDataByPage(people?.previous || "", -1);
   };
 
   return (
