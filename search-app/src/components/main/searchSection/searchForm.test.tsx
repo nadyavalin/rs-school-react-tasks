39afe7ee0c0239efbfb03578e14972d44120d7ab
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-07-28 21:12:20 +0300

    fix: correct error in searchForm component test, now coverage is 83,4825 interest

--- search-app/src/components/main/searchSection/searchForm.test.tsx
+++ search-app/src/components/main/searchSection/searchForm.test.tsx
@@ -1,15 +1,12 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { SearchForm } from "./searchForm";
-import { vi } from "vitest";
-import { beforeEach } from "vitest";
-import { getItemFromLocalStorage } from "../../../utils/utils";
 
 vi.mock("react-router-dom", () => ({
   useNavigate: vi.fn(),
 }));
 
 vi.mock("../../../utils/utils", () => ({
-  getItemFromLocalStorage: vi.fn(),
+  getItemFromLocalStorage: vi.fn(() => "initialSearchTerm"),
 }));
 
 describe("SearchForm", () => {
@@ -17,7 +14,6 @@ describe("SearchForm", () => {
     vi.mock("react-router-dom", () => ({
       useNavigate: vi.fn(),
     }));
-    getItemFromLocalStorage.mockReturnValueOnce("initialSearchTerm");
   });
 
   test("renders search form with initial value from localStorage", () => {
