4fba3d6afbb1031c533dd20c37f19d5ab09396db
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-08-06 20:39:42 +0300

    feat: add tests for index file

--- search-app/src/test/index.test.tsx
+++ search-app/src/test/index.test.tsx
@@ -1,11 +1,17 @@
 import { render } from "@testing-library/react";
 import { MainContent } from "../components/main/mainContent";
-import Home from "../pages/index";
+import Home, { getServerSideProps } from "../pages/index";
+import { PeopleResponse } from "../types/types";
+import { GetServerSidePropsContext } from "next";
 
 vi.mock("../components/main/mainContent", () => {
   return {
     MainContent: vi.fn(({ children }) => <div>{children}</div>),
   };
+});
+
+beforeEach(() => {
+  vi.clearAllMocks();
 });
 
 describe("Home Component", () => {
@@ -20,5 +26,71 @@ describe("Home Component", () => {
 
     expect(layout.type).toBe(MainContent);
     expect(layout.props.children).toBe(pageContent);
+  });
+
+  test("renders Home component with correct data", () => {
+    const peopleData = { results: [{ name: "Luke Skywalker" }] };
+
+    const layout = Home.getLayout(<div>{JSON.stringify(peopleData)}</div>);
+
+    expect(layout).toBeDefined();
+    expect(layout.props.children.props.children).toBe(JSON.stringify(peopleData));
+  });
+
+  test("renders empty when no people data is available", () => {
+    const emptyData = { results: [] };
+    const layout = Home.getLayout(<div>{JSON.stringify(emptyData)}</div>);
+
+    expect(layout).toBeDefined();
+    expect(layout.props.children.props.children).toBe(JSON.stringify(emptyData));
+  });
+});
+
+describe("getServerSideProps", () => {
+  test("returns peopleData when fetch is successful", async () => {
+    const mockData: PeopleResponse = {
+      count: 1,
+      next: "https://swapi.dev/api/people/?page=2",
+      previous: null,
+      results: [
+        {
+          name: "Luke Skywalker",
+          birth_year: "19BBY",
+          eye_color: "blue",
+          gender: "male",
+          hair_color: "blond",
+          height: "172",
+          mass: "77",
+          skin_color: "fair",
+          created: "2014-12-09T13:50:51.644000Z",
+          edited: "2014-12-20T21:17:56.891000Z",
+        },
+      ],
+    };
+
+    global.fetch = vi.fn().mockResolvedValueOnce({
+      ok: true,
+      json: vi.fn().mockResolvedValueOnce(mockData),
+    });
+
+    const result = await getServerSideProps({} as GetServerSidePropsContext);
+
+    expect(result).toEqual({
+      props: {
+        peopleData: mockData,
+      },
+    });
+  });
+
+  test("returns empty peopleData when fetch fails", async () => {
+    global.fetch = vi.fn().mockRejectedValueOnce(new Error("Fetch failed"));
+
+    const result = await getServerSideProps({} as GetServerSidePropsContext);
+
+    expect(result).toEqual({
+      props: {
+        peopleData: { results: [] },
+      },
+    });
   });
 });
