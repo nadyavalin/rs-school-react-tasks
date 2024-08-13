ab36846f54f5ee4c3758b34c6fd9c5c230f099f9
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-08-13 23:06:22 +0300

    init: add first file projects, setting up configuration files - vite, ts, eslint, prettier, husky

---
+++ forms/.eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "eslint-config-prettier",
+    "prettier",
+  ],
+  ignorePatterns: ["dist", "node_modules", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "react-compiler", "@typescript-eslint"],
+  rules: {
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "react/jsx-uses-vars": "error",
+    "react-compiler/react-compiler": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+    project: ["tsconfig.app.json"],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
